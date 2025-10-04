43ec7b4d48a921a5967aa6369da6961413d7bf76
gabriel fernando <gabrielfernando04407@gmail.com>
2025-10-03 22:14:49 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double nota1, nota2, nota3, media;
+
+Console.WriteLine("Média Aritmética de 3 numeros\n");
+
+Console.Write("Digite o primeiro numero: ");
+nota1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo numero: ");
+nota2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o terceiro numero: ");
+nota3 = Convert.ToDouble(Console.ReadLine());
+
+media = (nota1 + nota2 + nota3) / 3;
+
+Console.WriteLine($"\nMédia: {media:N1}");
